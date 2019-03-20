433723b48da150afec7994246c107f1f66f91b25
Aiza Tilenbaeva <tilenbaevaa@gmail.com>
2019-03-20 18:52:42 -0500

    edit

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,18 +1,18 @@
 pipeline {
     properties([parameters([string(defaultValue: '127.0.0.7', description: 'Please, provide an IP to host a Website', name: 'IP', trim: false)])])
-    stage ("Install git"){
+    stage("Install git"){
         sh "ssh ec2-user@${IP}    sudo yum install git python-pip -y"
     }
-    stage ("Clone a repo"){
+     stage("Clone a repo"){
         git "https://github.com/tilenbaeva/flask-examples.git"
     }
-    stage ("Copy files"){
+    stage("Copy files"){
         sh "scp * ec2-user@${IP}:/tmp/"
     }
-    stage ("Install requirements"){
+    stage("Install requirements"){
         sh "sudo ec2-user@${IP}     sudo  pip install -r /tmp/requirements.txt"
     }
-    stage ("Run App"){
+    stage("Run App"){
         sh "ssh ec2-user@${IP}  python /tmp/01-hello-world/hello.py"
     }
 }
