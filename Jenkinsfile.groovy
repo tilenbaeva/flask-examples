5abf8e483a6ecd219b71cb625f2b39ee9eea0309
Aiza Tilenbaeva <tilenbaevaa@gmail.com>
2019-03-20 18:57:56 -0500

    node

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,4 +1,4 @@
-pipeline {
+node {
     properties([parameters([string(defaultValue: '127.0.0.1', description: 'Please, provide an IP to host a Website', name: 'IP', trim: false)])])
     stage("Install git"){
         sh "ssh ec2-user@${IP}    sudo yum install git python-pip -y"
