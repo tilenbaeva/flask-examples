14ddbe43ba88edadb8d95fe265eb4d505985a96a
Aiza Tilenbaeva <tilenbaevaa@gmail.com>
2019-03-20 18:43:35 -0500

    Jenkinsfile.groovy initial file created

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,19 @@
+pipeline {
+    properties([parameters([string(defaultValue: '127.0.0.1', description: 'Please, provide an IP to build Website', name: 'IP', trim: false)])])
+
+    stage("Install git"){
+        sh "ssh ec2-user@IP    sudo yum install git python-pip -y"
+    }
+    stage ("Clone a repo"){
+        git "https://github.com/tilenbaeva/flask-examples.git"
+    }
+    stage ("Copy files"){
+        sh "scp * ec2-user@IP:/tmp/"
+    }
+    stage ("Install requirements"){
+        sh "sudo ec2-user@IP     sudo  pip install -r /tmp/requirements.txt"
+    }
+    stage ("Run App"){
+        sh "ssh ec2-user@IP  python /tmp/01-hello-world/hello.py"
+    }
+}
