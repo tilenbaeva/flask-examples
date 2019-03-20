0a7fff007a4613cb128410b36efd5e2e629092f5
Aiza Tilenbaeva <tilenbaevaa@gmail.com>
2019-03-20 18:50:51 -0500

    parameters edited

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,5 +1,5 @@
 pipeline {
-    properties([parameters([string(defaultValue: '127.0.0.1', description: 'Please, provide an IP to build Website', name: 'IP', trim: false)])])
+    properties([parameters([string(defaultValue: '127.0.0.7', description: 'Please, provide an IP to host a Website', name: 'IP', trim: false)])])
     stage ("Install git"){
         sh "ssh ec2-user@${IP}    sudo yum install git python-pip -y"
     }
